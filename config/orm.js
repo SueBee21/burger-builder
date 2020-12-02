76ad44b8b96ca16fa62dbca92ee639f1012bdf5f
SusanBeno <beno.susan@gmail.com>
2020-12-01 20:41:33 -0500

    updating orm

--- config/orm.js
+++ config/orm.js
@@ -1,10 +1,22 @@
 var connection = require("./connection");
 var orm = {
-    selectAll:function(tableName, cbModel){
-        connection.query("SELECT * FROM ??", tableName, function(error, data){
+    selectAll: function (tableName, cb) {
+        connection.query("SELECT * FROM ??", tableName, function (error, data) {
             if (error) throw error;
-            cbModel(data);
+            cb(data);
         });
-    }
+    },
+    insertOne: function (tableName, name, cb) {
+        connection.query('INSERT INTO burgers (burger_name, devoured) VALUES ("?", false);', name, function (error, data) {
+            if (error) throw error;
+            cb(data);
+        });
+    },
+    updateOne:function(tableName, id, cb) {
+        connection.query('UPDATE burgers SET devoured = true WHERE id = ?;', id, function(error, data){
+            if (error) throw error;
+            cb(data);
+        });
+    },
 };
 module.exports = orm;
