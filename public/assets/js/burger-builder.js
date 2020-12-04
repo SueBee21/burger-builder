23edee65f820479453f5e59432d05dab51232b69
SusanBeno <beno.susan@gmail.com>
2020-12-04 16:11:55 -0500

    adding bootstrap

--- public/assets/js/burger-builder.js
+++ public/assets/js/burger-builder.js
@@ -4,18 +4,18 @@
 
 // // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-    $(".devour").on("click", function(event) {
+    $(".devour-burger").on("click", function(event) {
       var id = $(this).data("id");
       var newDevoured = $(this).data("newDevoured");
   
-      var newDevouredState = {
+      var newDevouredList = {
         devoured: newDevoured
       };
   
 
       $.ajax("/api/burgers/" + id, {
         type: "PUT",
-        data: newDevouredState
+        data: newDevouredList
       }).then(
         function() {
           console.log("burger is devoured", newDevoured);
@@ -28,10 +28,9 @@ $(function() {
     $(".create-form").on("submit", function(event) {
 
       event.preventDefault();
-  
+
       var newBurgerList = {
-        name: $("#ca").val().trim(),
-        devoured: $("[name=devoured]:checked").val().trim()
+        name: $("#new").val().trim(),
       };
   
       // Send the POST request.
